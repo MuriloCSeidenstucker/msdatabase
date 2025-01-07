19eb7875bfe4c6e5bb2855f0b78b1203107b561f
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-01-07 16:49:20 -0300

    tutorial: Working with Transactions and the DBAPI

---
+++ msdatabase/main.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
+
+with engine.connect() as conn:
+    result = conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
+    conn.commit()
+print("#######################################################################")
+with engine.begin() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
+    )
+print("#######################################################################")
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
+print("#######################################################################")
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 2})
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
+print("#######################################################################")
+with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
+    )
+    conn.commit()
+print("#######################################################################")
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x} y: {row.y}")
+print("#######################################################################")
+with Session(engine) as session:
+    result = session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+    )
+    session.commit()
