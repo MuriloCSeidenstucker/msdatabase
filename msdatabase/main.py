a230d14756ede92f1bd5bac1e213906fd110787f
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-01-08 16:36:56 -0300

    tutorial: Working with Database Metadata - Emitting DDL to the database from an ORM mapping

--- msdatabase/main.py
+++ msdatabase/main.py
@@ -1,5 +1,7 @@
+from typing import List, Optional
+
 from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, relationship
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
 metadata_obj = MetaData()
@@ -26,22 +28,26 @@ with engine.connect() as conn:
         [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
     )
     conn.commit()
+    
 print("#######################################################################")
 with engine.begin() as conn:
     conn.execute(
         text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
         [{"x": 6, "y": 8}, {"x": 9, "y": 10}],
     )
+    
 print("#######################################################################")
 with engine.connect() as conn:
     result = conn.execute(text("SELECT x, y FROM some_table"))
     for row in result:
         print(f"x: {row.x} y: {row.y}")
+        
 print("#######################################################################")
 with engine.connect() as conn:
     result = conn.execute(text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 2})
     for row in result:
         print(f"x: {row.x} y: {row.y}")
+        
 print("#######################################################################")
 with engine.connect() as conn:
     conn.execute(
@@ -49,12 +55,14 @@ with engine.connect() as conn:
         [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
     )
     conn.commit()
+    
 print("#######################################################################")
 stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
 with Session(engine) as session:
     result = session.execute(stmt, {"y": 6})
     for row in result:
         print(f"x: {row.x} y: {row.y}")
+        
 print("#######################################################################")
 with Session(engine) as session:
     result = session.execute(
@@ -62,5 +70,39 @@ with Session(engine) as session:
         [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
     )
     session.commit()
-print("#######################################################################")
+    
+print("######################################## Emitindo DDL para o banco de dados ########################################")
 metadata_obj.create_all(engine)
+
+print("######################################## Estabelecendo uma Base Declarativa ########################################")
+class Base(DeclarativeBase):
+    pass
+print(Base.metadata)
+print(Base.registry)
+
+class User(Base):
+    __tablename__ = "user_account"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+    
+class Address(Base):
+    __tablename__ = "address"
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+    
+    user: Mapped[User] = relationship(back_populates="addresses")
+    
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+    
+print("######################### Emitindo DDL para o banco de dados a partir de um mapeamento ORM #########################")
+Base.metadata.create_all(engine)
