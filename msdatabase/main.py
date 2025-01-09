23826d8a6079a10f42bc7a1a9f64fc31a310dfbc
Murilo Seidenstucker <murilocampos.004@gmail.com>
2025-01-09 14:56:05 -0300

    tutorial: Working with Data - Insert...Returning

--- msdatabase/main.py
+++ msdatabase/main.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, text, MetaData, Table, Column, Integer, String, ForeignKey, insert, select, bindparam
 from sqlalchemy.orm import Session, DeclarativeBase, Mapped, mapped_column, relationship
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
@@ -106,3 +106,59 @@ class Address(Base):
     
 print("######################### Emitindo DDL para o banco de dados a partir de um mapeamento ORM #########################")
 Base.metadata.create_all(engine)
+
+print("###################################### A construção da expressão SQL insert() ######################################")
+stmt = insert(user_table).values(name="spongebob", fullname="Spongebob Squarepants")
+print(stmt)
+compiled = stmt.compile()
+print(compiled.params)
+
+print("############################################## Executando a Declaração #############################################")
+with engine.connect() as conn:
+    result = conn.execute(stmt)
+    conn.commit()
+    print(result.inserted_primary_key)    
+    
+print("############################# INSERT geralmente gera a cláusula “values” automaticamente ###########################")
+print(insert(user_table))
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(user_table),
+        [
+            {"name": "sandy", "fullname": "Sandy Cheeks"},
+            {"name": "patrick", "fullname": "Patrick Start"},
+        ],
+    )
+    conn.commit()
+    
+scalar_subq = (
+    select(user_table.c.id)
+    .where(user_table.c.name == bindparam("username"))
+    .scalar_subquery()
+)
+
+with engine.connect() as conn:
+    result = conn.execute(
+        insert(address_table).values(user_id=scalar_subq),
+        [
+            {
+                "username": "spongebob",
+                "email_address": "spongbob@sqlalchemy.org",
+            },
+            {"username": "sandy", "email_address": "sandy@sqlalchemy.org"},
+            {"username": "sandy", "email_address": "sandy@squirrelpower.org"},
+        ],
+    )
+    conn.commit()
+    
+print("################################################# INSERIR…RETORNANDO ###############################################")
+insert_stmt = insert(address_table).returning(
+    address_table.c.id, address_table.c.email_address
+)
+print(insert_stmt)
+
+select_stmt = select(user_table.c.id, user_table.c.name + "@aol.com")
+insert_stmt = insert(address_table).from_select(
+    ["user_id", "email_address"], select_stmt
+)
+print(insert_stmt.returning(address_table.c.id, address_table.c.email_address))
